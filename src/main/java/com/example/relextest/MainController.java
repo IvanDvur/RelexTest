36f9fb4e9a6de2ffaf4cf3c4193616caf5944d57
ivand <ivandvurechenskiy26@gmail.com>
2022-10-21 12:08:58 +0300

    added ascendingSeq method

--- src/main/java/com/example/relextest/MainController.java
+++ src/main/java/com/example/relextest/MainController.java
@@ -4,6 +4,8 @@ import com.example.relextest.service.RelexServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
@@ -17,11 +19,27 @@ public class MainController {
     }
 
     @GetMapping("/max")
-    public Integer getMaxValue(@RequestParam(name = "jsonUrl", required = true) String jsonUrl) {
+    public Integer getMaxValue(@RequestParam(name = "jsonUrl", required = false) String jsonUrl) {
         return relexService.getMaxValue(jsonUrl);
     }
+
     @GetMapping("/min")
-    public Integer getMinValue(@RequestParam(name = "jsonUrl", required = true) String jsonUrl) {
+    public Integer getMinValue(@RequestParam(name = "jsonUrl", required = false) String jsonUrl) {
         return relexService.getMinValue(jsonUrl);
+    }
+
+    @GetMapping("/average")
+    public Double getAverage(@RequestParam(name = "jsonUrl", required = false) String jsonUrl) {
+        return relexService.getAverage(jsonUrl);
+    }
+
+    @GetMapping("/mediane")
+    public Double getMediane(@RequestParam(name = "jsonUrl",required = false) String jsonUrl){
+        return relexService.getMediane(jsonUrl);
+    }
+
+    @GetMapping("/getAscend")
+    public List<Integer> getAscendingSeq(@RequestParam(name = "jsonUrl",required = false) String jsonUrl){
+        return relexService.getAscendingSequences(jsonUrl);
     }
 }
--- src/main/java/com/example/relextest/service/RelexServiceImpl.java
+++ src/main/java/com/example/relextest/service/RelexServiceImpl.java
@@ -1,66 +1,87 @@
 package com.example.relextest.service;
 
-
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class RelexServiceImpl implements RelexService {
+public class RelexServiceImpl {
 
     private List<String> fileNumbers;
-//    Поиск максимального значения
-    @Override
-    public Integer getMaxValue(String url) {
+
+    //  Создание коллекции из файла
+    public List<Integer> createListFromFile(String url) {
         try {
-            fileNumbers = Files.readAllLines(Path.of(url));
+            fileNumbers = Files.readAllLines(new File(url).toPath(), Charset.defaultCharset());
         } catch (IOException e) {
             System.out.println("Файл не найден");
         }
-        List<Integer> numbers = fileNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
+        return fileNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    //    Поиск максимального значения
+    public Integer getMaxValue(String url) {
+        List<Integer> numbers = createListFromFile(url);
         return Collections.max(numbers);
     }
 
-//    Поиск минимального значения
-    @Override
+    //    Поиск минимального значения
     public Integer getMinValue(String url) {
-        try {
-            List<String> fileNumbers = Files.readAllLines(new File(url).toPath(), Charset.defaultCharset());
-        } catch (IOException e) {
-            System.out.println("Файл не найден");
-        }
-        List<Integer> numbers = fileNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> numbers = createListFromFile(url);
         return Collections.min(numbers);
     }
 
-    @Override
-    public Integer getMediane(String url) {
-        return null;
+    // Поиск медианного значения
+    public Double getMediane(String url) {
+        List<Integer> numbers = createListFromFile(url);
+        numbers.sort(Comparator.naturalOrder());
+
+        System.out.println(numbers.size());
+        if (numbers.size() % 2 == 1) {
+            return Double.valueOf(numbers.get(numbers.size() / 2));
+        } else
+            return (double) ((numbers.get((numbers.size() / 2) - 1) + numbers.get(numbers.size() / 2)) / 2);
+
     }
 
-    @Override
+    //   Поиск среднего значения
     public Double getAverage(String url) {
-        return null;
+        List<Integer> numbers = createListFromFile(url);
+        return numbers.stream().mapToDouble(d -> d).average().orElse(0.0);
     }
 
-    @Override
-    public ArrayList<Integer[]> getAscendingSequences(String url) {
-        return null;
+    // Поиск самой длинной(ых) возрастающих последовательностей
+    public List<Integer> getAscendingSequences(String url) {
+        List<Integer> numbers = createListFromFile(url);
+        List<Integer> longestArray = new ArrayList<>();
+        List<Integer> currentArray = new ArrayList<>();
+        for (int i = 1; i < numbers.size(); i++) {
+            if (currentArray.isEmpty()) {
+                currentArray.add(numbers.get(i - 1));
+            }
+            if (numbers.get(i) > numbers.get(i - 1)) {
+                currentArray.add(numbers.get(i));
+            } else {
+                if (longestArray.size() < currentArray.size()) {
+                    longestArray.clear();
+                    longestArray.addAll(currentArray);
+                }
+                currentArray.clear();
+            }
+        }
+        return longestArray;
     }
 
-    @Override
+    //    Поиск самой длинной(ых) возрастающих последовательностей
     public ArrayList<Integer[]> getDescendingSequences(String url) {
         return null;
     }
-
-
 }
