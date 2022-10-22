3f5f00ae7979738e89fc5164ef252b4a539f29d0
ivand <ivandvurechenskiy26@gmail.com>
2022-10-22 22:19:50 +0300

    added app/json and app/xml support

--- src/main/java/com/example/relextest/MainController.java
+++ src/main/java/com/example/relextest/MainController.java
@@ -6,8 +6,11 @@ import com.example.relextest.service.RelexServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -22,10 +25,10 @@ public class MainController {
         this.relexService = relexService;
     }
 
-    @GetMapping(value = {"/makeoperation", "/makeoperation/{operation}"})
+    @GetMapping(value = {"/makeoperation", "/makeoperation/{operation}"},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseDto makeoperation(@RequestBody RequestDto requestDto,
                                      @PathVariable(name = "operation", required = false) String operation) {
-
 //        Если операция указана в JSON
         if (operation == null && requestDto.getOperation() != null) {
             if (requestDto.getOperation().equals("get_max"))
@@ -36,12 +39,11 @@ public class MainController {
                 return new ResponseDto(relexService.getAverage(requestDto.getPath()));
             if (requestDto.getOperation().equals("get_mediane"))
                 return new ResponseDto(relexService.getMediane(requestDto.getPath()));
-            if(requestDto.getOperation().equals("get_ascend"))
+            if (requestDto.getOperation().equals("get_ascend"))
                 return new ResponseDto(relexService.getAscendingSequences(requestDto.getPath()));
-            if(requestDto.getOperation().equals("get_descend"))
+            if (requestDto.getOperation().equals("get_descend"))
                 return new ResponseDto(relexService.getDescendingSequences(requestDto.getPath()));
         }
-
 //        Если операция указана в URL
         if (operation != null && requestDto.getOperation() == null) {
             if (operation.equals("get_max"))
@@ -52,13 +54,23 @@ public class MainController {
                 return new ResponseDto(relexService.getAverage(requestDto.getPath()));
             if (operation.equals("get_mediane"))
                 return new ResponseDto(relexService.getMediane(requestDto.getPath()));
-            if(operation.equals("get_ascend"))
+            if (operation.equals("get_ascend"))
                 return new ResponseDto(relexService.getAscendingSequences(requestDto.getPath()));
-            if(operation.equals("get_descend"))
+            if (operation.equals("get_descend"))
                 return new ResponseDto(relexService.getDescendingSequences(requestDto.getPath()));
         }
         return null;
     }
 
+
+//    @PostMapping(path = "/file",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    public ResponseDto uploadfile(@RequestParam("file")MultipartFile file){
+//        try(InputStream is = file.getInputStream()){
+//            return new ResponseDto(relexService.getMinValue(is.))
+//        }catch (IOException e){
+//            return new ResponseDto("Ошибка");
+//        }
+//        return null;
+//    }
 }
 
